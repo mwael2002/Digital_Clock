ec14d5d1ab4ceceda5292cf52b8f20fad8006645
mwael2002 <102371006+mwael2002@users.noreply.github.com>
2022-08-09 15:29:39 +0200

    Update DIO_private.h

--- Digital_Clock/DIO_private.h
+++ Digital_Clock/DIO_private.h
@@ -1,24 +1,32 @@
 /*
- * DIO_private.h
+ * DIO_register.h
  *
  *  Created on: Sep 10, 2021
  *      Author: mwael
  */
 
-#ifndef DIO_PRIVATE_H_
-#define DIO_PRIVATE_H_
-#define DIO_U8_DDRA *((U8*)0x3A)
-#define DIO_U8_DDRB *((U8*)0x37)
-#define DIO_U8_DDRC *((U8*)0x34)
-#define DIO_U8_DDRD *((U8*)0x31)
-#define DIO_U8_PORTA *((U8*)0x3B)
-#define DIO_U8_PORTB *((U8*)0x38)
-#define DIO_U8_PORTC *((U8*)0x35)
-#define DIO_U8_PORTD *((U8*)0x32)
-#define DIO_U8_PINA *((U8*)0x39)
-#define DIO_U8_PINB *((U8*)0x36)
-#define DIO_U8_PINC *((U8*)0x33)
-#define DIO_U8_PIND *((U8*)0x30)
+#ifndef DIO_REGISTER_H_
+#define DIO_REGISTER_H_
+
+#define DIO_U8_PINA *((volatile U8*)0x39)
+#define DIO_U8_DDRA *((volatile U8*)0x3A)
+#define DIO_U8_PORTA *((volatile U8*)0x3B)
+
+#define DIO_U8_PINB *((volatile U8*)0x36)
+#define DIO_U8_DDRB *((volatile U8*)0x37)
+#define DIO_U8_PORTB *((volatile U8*)0x38)
+
+#define DIO_U8_PINC *((volatile U8*)0x33)
+#define DIO_U8_DDRC *((volatile U8*)0x34)
+#define DIO_U8_PORTC *((volatile U8*)0x35)
+
+#define DIO_U8_PIND *((volatile U8*)0x30)
+#define DIO_U8_DDRD *((volatile U8*)0x31)
+#define DIO_U8_PORTD *((volatile U8*)0x32)
 
 
-#endif /* DIO_PRIVATE_H_ */
+
+
+
+
+#endif /* DIO_REGISTER_H_ */
