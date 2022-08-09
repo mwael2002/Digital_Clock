50f412983d74096e49022fdca69eb6c46d00a954
mwael2002 <102371006+mwael2002@users.noreply.github.com>
2022-08-09 15:33:33 +0200

    Update LCD_program.c

--- Digital_Clock/LCD_program.c
+++ Digital_Clock/LCD_program.c
@@ -7,74 +7,175 @@
  */
 #include"bit_calc.h"
 #include"STD.Types.h"
-#define F_CPU 8000000
 #include<avr/delay.h>
 #include"DIO_interface.h"
 #include"LCD_config.h"
 #include"LCD_private.h"
 #include"LCD_interface.h"
 
-void LCD_write_cmd(U8 cmd){
-	DIO_set_pin_value(LCD_control_port,LCD_Rs_pin,LOW);
-	LCD_write(cmd);
-}
-void LCD_write_data(U8 data){
-	DIO_set_pin_value(LCD_control_port,LCD_Rs_pin,HIGH);
-		LCD_write(data);
-}
-void LCD_write(U8 value){
-	DIO_set_pin_value(LCD_control_port,LCD_Rw_pin,LOW);
-	for(U8 i =0;i<8;i++){
-		DIO_set_pin_value(LCD_data_port,i,get_bit(value,i));
-	}
-	DIO_set_pin_value(LCD_control_port,LCD_E_pin,HIGH);
-	_delay_ms(1);
-	DIO_set_pin_value(LCD_control_port,LCD_E_pin,LOW);
+S8 current_address;
+S8 right_top_limit=init_right_top_limit;
+S8 right_bottom_limit=init_right_bottom_limit;
+S8 left_top_limit=init_left_top_limit;
+S8 left_bottom_limit=init_left_bottom_limit;
+
+
+void LCD_init(void){
+	DIO_set_port_direction(LCD_data_port,Port_8_pins_OUTPUT);
+	DIO_set_pin_direction(LCD_control_port,LCD_Rs_pin,OUTPUT);
+	DIO_set_pin_direction(LCD_control_port,LCD_Rw_pin,OUTPUT);
+	DIO_set_pin_direction(LCD_control_port,LCD_E_pin,OUTPUT);
+    _delay_ms(40);
+    LCD_send_cmd(0b00111000);
+    _delay_us(50);
+    LCD_send_cmd(0b00001101);
+    _delay_us(50);
+    LCD_send_cmd(1);
+    _delay_ms(2);
 }
 void LCD_clear(void){
-	LCD_write_cmd(1);
+	LCD_send_cmd(1);
 }
-U8 LCD_string_pos(char* string,U8 y,U8 x){
-	U8 local_address;
-	U8 error_state=0;
-	U8 i=0;
-	if((y<2)&&(x<16)){
-		local_address=(x+(y*64));
-		set_bit(local_address,7);
-		LCD_write_cmd(local_address);
-		while(string[i]!='\0'){
-			if(local_address==144){
-		local_address=192;
-		LCD_write_cmd(local_address);
-						}
-			LCD_write_data(string[i]);
-			i++; local_address++;
-		}
+
+void LCD_move_curser_right(void){
+    if((current_address!=39)&&(current_address!=103)){
+	if((current_address!=right_top_limit)&&(current_address!=right_bottom_limit)){
+		LCD_send_cmd(0b00010100);
+		current_address++;
 	}
+	else if((current_address==right_top_limit)||(current_address==right_bottom_limit)){
+			shift_display_left();
+		}
+
+}
+}
+void LCD_move_curser_left(void){
+    if((current_address!=0)&&(current_address!=64)){
+	if((current_address!=left_top_limit)&&(current_address!=left_bottom_limit)){
+		LCD_send_cmd(0b00010000);
+		current_address--;
+	}
+	else if((current_address==left_top_limit)||(current_address==left_bottom_limit)){
+            shift_display_right();
+		}
+
+}
+}
+void LCD_move_curser_bottom(void){
+	if((current_address+40)>103){
+		return;
+	}
+	current_address=current_address+64;
+	LCD_send_cmd(current_address+128);
+}
+void LCD_move_curser_top(void){
+	if((current_address-40)<0){
+		return;
+	}
+	current_address=current_address-64;
+	LCD_send_cmd(current_address+128);
+}
+
+
+void shift_display_left(void){
+	LCD_send_cmd(0b00011000);
+	right_top_limit++;
+	right_bottom_limit++;
+	left_top_limit++;
+	left_bottom_limit++;
+}
+void shift_display_right(void){
+	LCD_send_cmd(0b00011100);
+	right_top_limit--;
+	right_bottom_limit--;
+	left_top_limit--;
+	left_bottom_limit--;
+}
+
+
+
+void shift_entire_display_right_initial_pos(void){
+for(U8 count=0;count<24;count++){
+	LCD_send_cmd(0b00011100);
+}
+current_address=init_left_bottom_limit;
+
+right_top_limit=init_right_top_limit;
+right_bottom_limit=init_right_bottom_limit;
+left_top_limit=init_left_top_limit;
+left_bottom_limit=init_left_bottom_limit;
+}
+
+
+
+void LCD_write_char(U8 character){
+	DIO_set_pin_value(LCD_control_port,LCD_Rs_pin,HIGH);
+		LCD_send_data(character);
+}
+
+void LCD_write_string(const char* string){
+		    U8 count=0;
+			while(string[count]!='\0'){
+				if(current_address==39){
+					LCD_send_cmd(128+current_address);
+					LCD_write_char(string[count]);
+
+					shift_entire_display_right_initial_pos();
+
+					count++;
+					continue;
+				}
+				if((current_address==right_top_limit)||(current_address==right_bottom_limit)){
+				  shift_display_left();
+				  continue;
+				}
+				LCD_write_char(string[count]);
+				count++; current_address++;
+			}
+}
+U8 LCD_write_string_pos(const char* string,U8 y_pos,U8 x_pos){
+		U8 error_state=OK;
+		if((y_pos<2)&&(x_pos<16)){
+			current_address=(x_pos+(right_top_limit-15)+(y_pos*64));
+			LCD_send_cmd(128+current_address);
+			LCD_write_string(string);
+
+		}
 	else{
-		error_state=1;
+		error_state=NOK;
 	}
 	return error_state;
 }
-U8 LCD_clear_pos(U8 y,U8 x){
-	U8 local_address;
-		U8 error_state=0;
-		if((y<2)&&(x<16)){
-			local_address=(x+(y*64));
-			set_bit(local_address,7);
-			LCD_write_cmd(local_address);
-			LCD_write_data(254);
+void LCD_write_no(U32 num){
+	U8 rem,arr_index=0;
+		static char str[15];
+		static char str_2[15];
+		if(num==0){
+			str_2[arr_index]='0';
+			arr_index++;
+			str_2[1]='\0';
 		}
 		else{
-			error_state=1;
+			while(num!=0){
+		rem=num%10;
+		str[arr_index]=rem+'0';
+		arr_index++;
+		num=num/10;
 		}
-		return error_state;
-
+			str[arr_index]='\0';
+			U8 j=0;
+			for(S8 i=(arr_index-1);i>=0;i--){
+				str_2[j]=str[i];
+				j++;
+			}
+			str_2[j]='\0';
+		}
+		LCD_write_string(str_2);
 }
-void LCD_write_no(U32 num,U8 y, U8 x){
+void LCD_write_no_pos(U32 num,U8 y_pos, U8 x_pos){
 	U8 rem,arr_index=0;
-	static U8 str[15];
-	static U8 str_2[15];
+	static char str[15];
+	static char str_2[15];
 	if(num==0){
 		str_2[arr_index]='0';
 		arr_index++;
@@ -95,18 +196,20 @@ void LCD_write_no(U32 num,U8 y, U8 x){
 		}
 		str_2[j]='\0';
 	}
-	LCD_string_pos(str_2,y,x);
+	LCD_write_string_pos(str_2,y_pos,x_pos);
 }
-void LCD_init(void){
-	DIO_set_port_direction(LCD_data_port,DIO_Max_Port_direction);
-	DIO_set_pin_direction(LCD_control_port,LCD_Rs_pin,OUTPUT);
-	DIO_set_pin_direction(LCD_control_port,LCD_Rw_pin,OUTPUT);
-	DIO_set_pin_direction(LCD_control_port,LCD_E_pin,OUTPUT);
-    _delay_ms(40);
-    LCD_write_cmd(0b00111000);
-    _delay_us(50);
-    LCD_write_cmd(0b00001100);
-    _delay_us(50);
-    LCD_write_cmd(0b00000001);
-    _delay_ms(2);
+
+
+
+
+static void LCD_send_data(U8 data){
+	DIO_set_pin_value(LCD_control_port,LCD_Rw_pin,LOW);
+	DIO_set_port_value(LCD_data_port,data);
+	DIO_set_pin_value(LCD_control_port,LCD_E_pin,HIGH);
+	_delay_ms(1);
+	DIO_set_pin_value(LCD_control_port,LCD_E_pin,LOW);
+}
+static void LCD_send_cmd(U8 cmd){
+	DIO_set_pin_value(LCD_control_port,LCD_Rs_pin,LOW);
+	LCD_send_data(cmd);
 }
