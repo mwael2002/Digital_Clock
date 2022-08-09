5ea792b4fee26272f63675f3b336f00ccbd3a1e1
mwael2002 <102371006+mwael2002@users.noreply.github.com>
2022-08-09 15:36:39 +0200

    Update Timer_0_ov_program.c

--- Digital_Clock/Timer_0_ov_program.c
+++ Digital_Clock/Timer_0_ov_program.c
@@ -4,44 +4,35 @@
  *  Created on: Oct 22, 2021
  *      Author: mwael
  */
-#include"STD.Types.h"
-#include"bit_calc.h"
+#include"D:/C/STD.Types.h"
+#include"D:/C/bit_calc.h"
 #include"Timer_0_ov_private.h"
 #include"Timer_0_ov_interface.h"
-void (*ptr_timer) (void);
+#include"LCD_interface.h"
+
+void (*ptr_timer) (void)=0;
 U16 TCNT0_register_value;
-U16 restart_count;
-
-extern U8 sec;
-
 
 
 void Timer_ov_init(U16 time_milliseconds){
     time_calculate(time_milliseconds);
 	TCNT0 =TCNT0_register_value;
+
 	TCCR0 = 0b00000010;
 	//prescaler=8
 	//Normal mode
 	set_bit(TIMSK,0); // Interrupt Enable
 	set_bit(SREG,7); // Enable GIE
 }
-void time_calculate(U16 time_millisecond){
+U32 time_calculate(U16 time_millisecond){
     F32 count;
-    count=(1000000.0*time_millisecond/256*1000);
-    restart_count=count;
-    TCNT0_register_value=(256*(1-(count-restart_count)));
-    restart_count++;
+    count=(1000.0*time_millisecond)/256;
+    TCNT0_register_value=(256*(1-(count-(U32)count)));
+    return ((U32)count+1);
 }
-
-void time_value(void){
-	static U16 count=0;
-	count++;
-	if(count==restart_count){
-		sec++;
-		count=0;
-		TCNT0=TCNT0_register_value;
-	}
-	}
+void restart_count(void){
+	TCNT0=TCNT0_register_value;
+}
 
 
 void callback(void(*ptr)(void)){
