9344f93788d6301530123954a3249cb0c18eb480
mwael2002 <102371006+mwael2002@users.noreply.github.com>
2022-08-09 15:35:05 +0200

    Update Timer_0_ov_interface.h

--- Digital_Clock/Timer_0_ov_interface.h
+++ Digital_Clock/Timer_0_ov_interface.h
@@ -8,8 +8,8 @@
 #ifndef TIMER_0_OV_INTERFACE_H_
 #define TIMER_0_OV_INTERFACE_H_
 void Timer_ov_init(U16 time_millisec);
-void time_calculate(U16 time_millisecond);
-void time_value(void);
+U32 time_calculate(U16 time_millisecond);
+void restart_count(void);
 void callback(void(*ptr)(void));
 void __vector_11(void) __attribute__((signal));
 #endif /* TIMER_0_OV_INTERFACE_H_ */
